86661c2764fe72b01ed1b7496f0b10cad0602659
Carlos-SSH <carlos.anezeh@me.com>
2021-05-03 23:31:05 -0500

    Add name for players.
    Set basic style with bootstrap.

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+let playerOne = prompt("Player One Name: ");
+document.write("Player one: " + playerOne);
+
+let playerTwo = prompt("Player Two Name: ");
+document.write("Player Two: " + playerTwo);
